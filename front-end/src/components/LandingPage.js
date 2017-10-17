f2b3330b8ddee1a3a9f5bd7617f85c2c225a77de
paspheeris <pa.spheeris@gmail.com>
2017-10-17 14:37:36 -0700

    FIX clean up actions.js;REMOVE FreeCodeCamp from landing page title

--- front-end/src/components/LandingPage.js
+++ front-end/src/components/LandingPage.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import auth from '../auth/Auth';
-import { Segment, Icon, Divider, Header } from 'semantic-ui-react';
-
-// const auth = new Auth();
+import { Divider } from 'semantic-ui-react';
 
 const LandingPage = () => {
   return (
     <div className="LandingPage-wrapper">
-      <h1 className="LandingPage-topText">FreeCodeCamp Polling App</h1>
+      <h1 className="LandingPage-topText">Polling App</h1>
       <Divider />
-          <ul className="features-list">
-            <li>Create and share polls with your friends</li>
-            <li>Vote on other's polls</li>
-            <li>View and edit the polls you've created</li>
-            <li>No registration required: log on with a Google or GitHub account</li>
-          </ul>
-        <img className="ballotBox-image" src='ballotBox.svg' alt="<div>Icons made by <a href='http://www.freepik.com' title='Freepik'>Freepik</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0 BY</a></div>'" />
-      </div>
+      <ul className="features-list">
+        <li>Create and share polls with your friends</li>
+        <li>Vote on other's polls</li>
+        <li>View and edit the polls you've created</li>
+        <li>No registration required: log on with a Google or GitHub account</li>
+      </ul>
+      <img className="ballotBox-image" src='ballotBox.svg' alt="<div>Icons made by <a href='http://www.freepik.com' title='Freepik'>Freepik</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0 BY</a></div>'" />
+    </div>
   )
 }
 
